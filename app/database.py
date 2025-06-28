5d28857d229bc3aad81c0f6a4498a11dce2edb79
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2025-06-29 00:52:49 +0530

    Refactor database operations to ensure proper table creation, data insertion, and connection management

--- app/database.py
+++ app/database.py
@@ -5,13 +5,37 @@ connection = sqlite3.connect("sqlite.db")
 
 cursor = connection.cursor()
 
-# create table
-cursor.execute("""CREATE TABLE shipment(
+# 1.create table
+cursor.execute("""CREATE TABLE IF NOT EXISTS shipment(
     id INTEGER, 
     content TEXT,
     weight REAL,
     status TEXT  
-    )""") 
-    
-    
+    )""")
+# 2.add shipment data
+cursor.execute("""
+               INSERT INTO shipment
+               VALUES(12345, "Palm Trees", 8.5, 'placed')          
+               """)
+connection.commit()
+
+# 3. Read a shipment by id
+
+cursor = cursor.execute("""
+                        SELECT * FROM shipment
+                        """)
+result = cursor.fetchall()
+print(result)
+
+
+# 4. delete the shipment.id
+
+cursor.execute("""
+               DELETE from  shipment where id = 12345
+               
+               """)
+
+
+connection.commit()
+
 connection.close()
