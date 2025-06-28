2d2c91ff4e0d23d26fd2e6a51e5c54bdd3524121
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2025-06-29 02:01:04 +0530

    Refactor database operations to improve table structure, data insertion, and update logic

--- app/database.py
+++ app/database.py
@@ -1,41 +1,59 @@
 import sqlite3
 
-
 connection = sqlite3.connect("sqlite.db")
-
 cursor = connection.cursor()
 
-# 1.create table
-cursor.execute("""CREATE TABLE IF NOT EXISTS shipment(
-    id INTEGER, 
-    content TEXT,
-    weight REAL,
-    status TEXT  
-    )""")
-# 2.add shipment data
+
+
+
+# 1. Create table
 cursor.execute("""
-               INSERT INTO shipment
-               VALUES(12345, "Palm Trees", 8.5, 'placed')          
-               """)
+    CREATE TABLE IF NOT EXISTS shipment(
+        id INTEGER PRIMARY KEY, 
+        content TEXT,
+        weight REAL,
+        status TEXT  
+    )
+""")
+
+# 2. Add shipment data
+cursor.execute("""
+    INSERT INTO shipment
+    VALUES(12345, "Palm Trees", 8.5, 'placed')          
+""")
 connection.commit()
 
-# 3. Read a shipment by id
-
+# 3. Read all shipments
 cursor = cursor.execute("""
-                        SELECT * FROM shipment
-                        """)
+    SELECT * FROM shipment
+""")
 result = cursor.fetchall()
 print(result)
 
-
-# 4. delete the shipment.id
-
+# 4. Delete the shipment by id
 cursor.execute("""
-               DELETE from  shipment where id = 12345
-               
-               """)
-
-
+    DELETE from shipment where id = 12345
+""")
 connection.commit()
+
+
+# 5. update a shipment
+id = 12345
+status = "in_transit"
+cursor.execute("""
+    UPDATE shipment
+    SET  status = :status
+    WHERE id = :id
+""" , {"status": status, "id": id})
+connection.commit()
+
+
+
+# 6. DROP SHIPMENT
+cursor.execute("""
+    DROP TABLE IF EXISTS shipment
+""")
+connection.commit()
+
 
 connection.close()
