e689ad8fb76b3eaf70b882f9394c8122e1bd06f8
Kartikeya Singh <105882265+karkau123@users.noreply.github.com>
2025-07-05 00:21:20 +0530

    Refactor database management to encapsulate connection and table creation logic

--- app/database.py
+++ app/database.py
@@ -1,59 +1,97 @@
 import sqlite3
+from .schemas import ShipmentCreate
 
-connection = sqlite3.connect("sqlite.db")
-cursor = connection.cursor()
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect("sqlite.db")
+        self.cur = self.conn.cursor()
+        self.create_table("shipment")
+        
+    def create_table(self, name: str):
+        self.cur.execute("""
+            CREATE TABLE IF NOT EXISTS ?(
+                id INTEGER PRIMARY KEY, 
+                content TEXT,
+                weight REAL,
+                status TEXT  
+            )""",
+            (name,)
+            )
+    
+    def create(self ,shipment: ShipmentCreate)->int:
+        self.curr.execute("SELECT MAX(id) FROM shipment")
+        result = self.curr.fetchone()
+        
+        new_id = result[0]+1
+        
+        self.cur.execute("""
+                        INSERT INTO shipment
+                        VALUES(:id, :content, :weight,:status)
+                    """ , {
+                         "id": new_id,
+                         **shipment.model_dump(),    
+                         "status" : "placed"
+                    })
+        self.conn.commit()
+        return new_id
+        
+        
+    def get(self , id : int):
+        self.cur = self.cur.execute("""
+            SELECT * FROM shipment
+            where id = ?
+        """, (id,) )
+        row = self.cur.fetchall()
+        return {
+             "id" : row[0],
+             "content" : row[1],
+             "weight" : row[2],
+             "status" : row[3]
+        }
+        
+
+
+ 
 
 
 
 
-# 1. Create table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS shipment(
-        id INTEGER PRIMARY KEY, 
-        content TEXT,
-        weight REAL,
-        status TEXT  
-    )
-""")
 
-# 2. Add shipment data
-cursor.execute("""
-    INSERT INTO shipment
-    VALUES(12345, "Palm Trees", 8.5, 'placed')          
-""")
-connection.commit()
 
+
+ 
+ 
 # 3. Read all shipments
-cursor = cursor.execute("""
-    SELECT * FROM shipment
-""")
-result = cursor.fetchall()
-print(result)
+# cursor = cursor.execute("""
+#     SELECT * FROM shipment
+# """)
+# result = cursor.fetchall()
+# print(result)
 
 # 4. Delete the shipment by id
-cursor.execute("""
-    DELETE from shipment where id = 12345
-""")
-connection.commit()
+# cursor.execute("""
+#     DELETE from shipment where id = 12345
+# """)
+# connection.commit()
 
 
 # 5. update a shipment
-id = 12345
-status = "in_transit"
-cursor.execute("""
-    UPDATE shipment
-    SET  status = :status
-    WHERE id = :id
-""" , {"status": status, "id": id})
-connection.commit()
+# id = 12345
+# status = "in_transit"
+# cursor.execute("""
+#     UPDATE shipment
+#     SET  status = :status
+#     WHERE id = :id
+# """ , {"status": status, "id": id})
+# connection.commit()
 
 
 
 # 6. DROP SHIPMENT
-cursor.execute("""
-    DROP TABLE IF EXISTS shipment
-""")
-connection.commit()
+# cursor.execute("""
+#     DROP TABLE IF EXISTS shipment
+# """)
+# connection.commit()
 
 
-connection.close()
+# connection.close()
